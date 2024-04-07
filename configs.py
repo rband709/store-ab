d25e3bb5e3970b568e84b02ef69158b9d9a5979a
rband709 <119666248+rband709@users.noreply.github.com>
2024-04-07 14:58:43 +0330

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,14 +4,14 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	API_ID = int(os.environ.get("API_ID", "3335796"))
+	API_HASH = os.environ.get("API_HASH","138b992a0e672e8346d8439c3f42ea78")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN","5088657122:AAELk-O6R8rYxzqXNvWWRhtl2O0-FNLwHS0")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME","fi2li123robot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001792962793"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "763990585"))
+	DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://abirhasan2005:abirhasan@cluster0.i6qzp.mongodb.net/cluster0?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001792962793")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
